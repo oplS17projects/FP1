a48873597cd2ff1638ea42e6dc1e376df1eea211
Darraptor <Hussaindar1993@yahoo.com>
2017-03-12 22:44:37 -0400

    Update connect4.rkt
    Changed variable names for readability

--- connect4.rkt
+++ connect4.rkt
@@ -6,12 +6,16 @@
 (define connect4yellowpiece (circle 10 "solid" "yellow"))
 (define connect4redpiece (circle 10 "solid" "red"))
 
-(define square (square 40 'outline 'black))
-(define rectangle (beside square square square square square square square))
+(define square1 (square 40 'outline 'black))
+(define rectangle (beside square1 square1 square1 square1 square1 square1 square1))
 
-(overlay/align "middle" "bottom" connect4yellowpiece r1)
-(overlay/align "middle" "bottom" connect4redpiece r1)
-(overlay/align "middle" "bottom" connect4yellowpiece r1)
+(overlay/align "middle" "bottom" connect4yellowpiece rectangle)
+(overlay/align "middle" "bottom" connect4redpiece rectangle)
+(overlay/align "middle" "bottom" connect4yellowpiece rectangle)
+(overlay/align "middle" "bottom" connect4redpiece rectangle)
+(overlay/align "middle" "bottom" connect4yellowpiece rectangle)
+(overlay/align "middle" "bottom" connect4redpiece rectangle)
+(overlay/align "middle" "bottom" connect4yellowpiece rectangle)
 
 
 
