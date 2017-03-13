82a4e85a1bfd18a511262440d3a52cf3e1772873
Tensai66 <Tensai66@users.noreply.github.com>
2017-03-13 00:44:59 -0400

    commiting rkt test file first time

---
+++ explorations.rkt
@@ -0,0 +1,19 @@
+#lang slideshow
+
+(slide
+  #:title "Hello World Slide"
+  (item "Hello World")
+  (item "This is another test to see what happens")
+  (item "Having an item tag makes more bullets"))
+
+(slide
+ #:title "More slides test"
+ (item "Having another slide function makes another slide")
+ (item "This is a way to make the slideshow")
+ (item "Also if you press 's' you can go to the next slide"))
+
+(slide
+ #:title "Testing commands"
+ (item "If you press 'a' you can go to the previous slide")
+ (item "This was my explorations test for slideshow library")
+ (item "It is easy to use and also powerful with the built in features"))
