281afaa52d4a3b6972e7408a7c6802d95d350b70
LeonKang978 <lkang978@gmail.com>
2017-03-12 21:35:50 -0400

    Update FP1.rkt

--- FP1.rkt
+++ FP1.rkt
@@ -3,20 +3,16 @@
 ;Email: leon_kang@student.uml.edu
 ;FP1
 (require math/array)
-
-
-
-
 ;build an array of natural numbers [0, 1, 2, 3, ..., array-end] until array-end then
 ;apply the array-map to this array with the argument function
-(define (my-array array-end func)
+(define (my-array numElements func)
   (define (my-array-helper count)
-     (if(> count array-end)
-        '()
-        (cons count (my-array-helper (+ count 1)))
-      )
-     )
-(array-map func (list->array (my-array-helper 0)))
-     
+    (if(= count numElements)
+       '()
+       (cons count (my-array-helper (+ count 1)))
+       )
+    )
+  (array-map func (list->array (my-array-helper 0)))
+
   )
   
