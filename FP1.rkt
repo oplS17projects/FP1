41485a403c5d10f6beaf8f9111eb1701ccbfd3eb
Marittya "Mike" Keu <Marittyakeu@gmail.com>
2017-03-12 23:35:31 -0400

    Add files via upload

---
+++ FP1.rkt
@@ -0,0 +1,28 @@
+#lang racket
+(require math/base)
+(require math/matrix)
+
+;generates a random number from 0 to x
+(define (randNum x)
+  (random-integer 0 x))
+
+;Pythagorean Theorem
+(define (pyt a b)
+  (+ (sqr a) (sqr b)))
+
+(define (stoi x)
+  (string->number x))
+
+(define (itos x)
+  (number->string x))
+
+;defining a ouple of functions
+(define A (matrix ([1 2]
+                   [4 5])))
+(define B (matrix ([2 1]
+                   [2 5])))
+
+;test it by using these commands in REPL
+;(matrix+ A B)
+;(matrix- A B)
+;(matrix* A B)
